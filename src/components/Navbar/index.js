c0ef6a4813ad4fe7c2da0a5b1684e42e5d750609
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-10-01 14:23:05 -0300

    Navbar y SectionMain

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+export default function Navbar() {
+	return (
+		<nav className='Navbar'>
+			<ul>
+				<li>Home</li>
+				<li>Proyectos</li>
+				<li>Skills</li>
+				<li>Contacto</li>
+			</ul>
+		</nav>
+	)
+}
---
+++ src/components/SectionMain/index.js
@@ -0,0 +1,6 @@
+import React, { Children } from 'react'
+import './styles.css'
+
+export default function SectionMain({ children }) {
+	return <section className='SectionMain'>{children}</section>
+}
