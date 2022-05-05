a6c87c499496b346a35f3603bdf5037ce43931ae
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-05-05 13:05:05 -0300

    Lazy loading de las imagenes de los proyectos

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 import './styles.css'
 
 import TitleHeader from '../TitleHeader'
@@ -9,7 +11,7 @@ export default function Project({ id, title, img, icons, children, links }) {
 	return (
 		<section className='Project' id={id}>
 			<TitleHeader>{title}</TitleHeader>
-			<img className='Project__img' src={img} alt={title} />
+			<LazyLoadImage className='Project__img' src={img} alt={title} effect='blur' />
 			<div className='Project__info'>
 				<div className='Project__stack'>
 					{icons.map((icon) => (
